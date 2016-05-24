2fe717f70466451c61d24edd599791ca45edb3c3
Marcel Hild <hild@b4mad.net>
2016-05-24 12:18:56 +0200

    wip

--- gems/manageiq-providers-amazon/spec/models/manageiq/providers/amazon/instance_types_spec.rb
+++ gems/manageiq-providers-amazon/spec/models/manageiq/providers/amazon/instance_types_spec.rb
@@ -1,10 +1,16 @@
 describe ManageIQ::Providers::Amazon::InstanceTypes do
   class Attribute
     attr_accessor :key, :key_other, :processor, :other_proc
-    def initialize(key, key_other = nil, &processor)
-      self.key = key
-      self.key_other = key_other || key
-      self.processor = processor
+    attr_accessor :convert, :other_convert
+    def initialize(key)
+      if key.is_a? Hash
+        self.key, self.key_other = key.first
+      else
+        self.key = self.key_other = key
+      end
+      yield self if block_given?
+      # self.key_other = key_other || key
+      # self.processor = processor
     end
 
     def other_proc(&block)
@@ -20,8 +26,12 @@ describe ManageIQ::Providers::Amazon::InstanceTypes do
       end
     end
 
-    def same?(a, other)
-      fetch(a, key) == fetch(other, key_other)
+    def same?(a, b)
+      av, bv = a[key], b[key_other]
+      av = convert.to_proc.call(av) if convert
+      bv = convert.to_proc.call(bv) if convert
+      bv = other_convert.to_proc.call(bv) if other_convert
+      av == bv
     end
 
     def format(instance)
@@ -29,10 +39,17 @@ describe ManageIQ::Providers::Amazon::InstanceTypes do
     end
   end
 
-  def format_instance(instance, attributes)
+  def format_instance(instance, current_instance, attributes)
     puts %Q("#{instance[:instance_type]}" => {)
     attributes.each do |key|
-      puts key.format(instance)
+      if current_instance
+        unless key.same?(current_instance, instance)
+          puts key.format(instance)
+          puts %Q(# was: #{current_instance[key.key]})
+        end
+      else
+        puts key.format(instance)
+      end
     end
     puts '}'
   end
@@ -41,10 +58,16 @@ describe ManageIQ::Providers::Amazon::InstanceTypes do
     require 'open-uri'
 
     attributes = [
-      Attribute.new(:name, :instance_type),
-      Attribute.new(:family) {|a| a.downcase},
-      Attribute.new(:description, :pretty_name) {|a| a.downcase},
-      Attribute.new(:memory).other_proc{|b| b.to_f.gigabytes }
+      Attribute.new(:name => :instance_type),
+      Attribute.new(:family) {|a| a.convert = :downcase },
+      Attribute.new(:description => :pretty_name) {|a| a.convert = :downcase},
+      Attribute.new(:memory) do |a|
+        a.convert = :to_f
+        a.other_convert = Proc.new do |v|
+          v.to_f.gigabytes
+        end
+      end,
+      Attribute.new(:vcpu => :vCPU),
     ]
 
     find_instance = Proc.new do |o|
@@ -53,11 +76,16 @@ describe ManageIQ::Providers::Amazon::InstanceTypes do
       end
     end
 
+    current_instance = Proc.new do |o|
+      described_class.all.find{|i| i[:name] == o[:instance_type]}
+    end
+
     # instances = YAML.safe_load(open('https://raw.githubusercontent.com/powdahound/ec2instances.info/master/www/instances.json').read)
     instances = YAML.safe_load(open('/tmp/instances.json').read)
     instances.each do |instance|
       instance.deep_symbolize_keys!
-      find_instance.call(instance) or format_instance(instance, attributes)
+      find_instance.call(instance) or
+        format_instance(instance, current_instance.call(instance), attributes)
     end
 
   end
